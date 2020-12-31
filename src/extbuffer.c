2e41877dcea31981f758efab3f43cb680a27d237
Benjamin Crawford <crawford.benjamin15@gmail.com>
2020-12-31 19:47:40 +0000

    Initial commit 2
    Signed-off-by: Benjamin Crawford <crawford.benjamin15@gmail.com>

--- src/extbuffer.c
+++ src/extbuffer.c
@@ -14,13 +14,23 @@
 #include <stdlib.h>
 
 typedef struct ceb_buffer_sz {
-
+	size_t sz;
+	size_t used_sz;
+	size_t *sz_buf;
+	int rsz_ratio;
 } _ceb_buffer_sz_t;
 
 typedef struct ceb_buffer {
-	size_t sz; // Size of this buffer in multiples of sizeof(char)
+	size_t sz; 		// Size of this buffer in multiples of sizeof(char)
+	size_t used_sz;		// Occupied contiguous space inside this buffer.
+	void *buf;		// Beginning of the memory region where this buffer is stored.
 	_ceb_buffer_sz_t types; // Similar buffer structure designed only to contain sequential type sizes as they are contained within this buffer.
 	int rsz_ratio;          // Ratio /100 when the buffer should be resized.
 } ceb_buffer_t;
 
+char ceb_append_object(ceb_buffer_t *buf, void *obj_ref, size_t sz) {
+	if(buf->sz - buf->used_sz < sz) { return NULL; } // In future, this will resize the underlying buffer.
+	
+	*(buf + used_sz) = *obj_ref; // Note that this assumes that the caller was truthful about size. 
+}
 
