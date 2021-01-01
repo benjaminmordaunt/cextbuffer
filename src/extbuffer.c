246ad93dec99784e731b880962aed6d897950a4b
Benjamin Crawford <crawford.benjamin15@gmail.com>
2021-01-01 22:21:56 +0000

    Add required type casts and simple Makefile
    Signed-off-by: Benjamin Crawford <crawford.benjamin15@gmail.com>

--- src/extbuffer.c
+++ src/extbuffer.c
@@ -14,6 +14,8 @@
 #define CEXTARG(x) x, sizeof(x)
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
 typedef struct ceb_buffer_sz {
 	size_t sz;
@@ -33,21 +35,20 @@ typedef struct ceb_buffer {
 char ceb_append_object(ceb_buffer_t *buf, void *obj_ref, size_t sz) {
 	if(buf->sz - buf->used_sz < sz) { return 1; } // In future, this will resize the underlying buffer.
 	
-	/* NOTE: memcpy not used here as sizes pre-verified */	
-	*(buf->buf + buf->used_sz) = *obj_ref; // Note that this assumes that the caller was truthful about size. 
+	memcpy((void*)((uintptr_t)buf->buf + buf->used_sz), obj_ref, sz); // Note that this assumes that the caller was truthful about size. 
 	buf->used_sz += sz;
 
 	// Add type size
 	// TODO: Handle case where _ceb_buffer_sz_t runs out of size_t slots
 	int cbtb_idx = (buf->types).used_sz / sizeof(size_t);
-	(buf->types).sz_buf[cbtb_idx] = sz;
+	(buf->types).buf[cbtb_idx] = sz;
 	return 0;
 }
 
 char _ceb_remove_type_sz(_ceb_buffer_sz_t *buf, size_t idx) {
-	void *seek_ptr = buf->buf + idx * sizeof(size_t);
+	void *seek_ptr = (void*)((uintptr_t)buf->buf + idx * sizeof(size_t));
 
-	memmove(seek_ptr, seek_ptr + sizeof(size_t), buf->sz - (seek_ptr - buf->buf));
+	memmove(seek_ptr, (void*)((uintptr_t)seek_ptr + (uintptr_t)sizeof(size_t)), buf->sz - ((uintptr_t)seek_ptr - (uintptr_t)buf->buf));
 	buf->used_sz -= sizeof(size_t);
 	return 0;
 }
@@ -58,11 +59,11 @@ char ceb_remove_object(ceb_buffer_t *buf, size_t idx) {
 	
 	// Iterate through type lengths
 	for(i = 0; i < idx; i++) {
-		seek_ptr += (buf->types).sz_buf[i]; // Consider using accumulating lengths (i.e. [a, a+b, a+b+c], instead of [a, b, c] to mitigate need for long loops)
+		seek_ptr = (void*)((uintptr_t)seek_ptr + (uintptr_t)(buf->types).buf[i]); // Consider using accumulating lengths (i.e. [a, a+b, a+b+c], instead of [a, b, c] to mitigate need for long loops)
 	}
 
-	memmove(seek_ptr, (seek_ptr + (buf->types).sz_buf[i]), buf->types.sz - (seek_ptr - buf->buf));
-	buf->used_sz -= (buf->types).sz_buf[i];
+	memmove(seek_ptr, (void*)((uintptr_t)seek_ptr + (uintptr_t)(buf->types).buf[i]), buf->types.sz - ((uintptr_t)seek_ptr - (uintptr_t)buf->buf));
+	buf->used_sz -= (buf->types).buf[i];
 
 	// Call to remove corresponding type size
 	_ceb_remove_type_sz(&buf->types, idx);
