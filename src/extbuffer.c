f2071ed7c3a3314aca9c3acfb66e23342ab462a4
Benjamin Crawford <crawford.benjamin15@gmail.com>
2021-01-02 22:40:15 +0000

    Fill header file
    Signed-off-by: Benjamin Crawford <crawford.benjamin15@gmail.com>

--- src/extbuffer.c
+++ src/extbuffer.c
@@ -11,20 +11,7 @@
 	#define CEXTBUFFER_ALIGN 1
 #endif /* CEXTBUFFER_ALIGN */
 
-typedef struct ceb_buffer_sz {
-	size_t sz;
-	size_t used_sz;
-	size_t *buf;
-	int rsz_ratio;
-} _ceb_buffer_sz_t;
-
-typedef struct ceb_buffer {
-	size_t sz; 		// Size of this buffer in multiples of sizeof(char)
-	size_t used_sz;		// Occupied contiguous space inside this buffer.
-	void *buf;		// Beginning of the memory region where this buffer is stored.
-	_ceb_buffer_sz_t types; // Similar buffer structure designed only to contain sequential type sizes as they are contained within this buffer.
-	int rsz_ratio;          // Ratio /100 when the buffer should be resized.
-} ceb_buffer_t;
+#include "extbuffer.h"
 
 char ceb_init_buffer(ceb_buffer_t *buf, size_t init_sz) {
 	buf->buf = calloc(init_sz, 1);
--- src/extbuffer.h
+++ src/extbuffer.h
@@ -16,4 +16,29 @@
 #include <string.h>
 #include <stdint.h>
 
+/* Types */
+
+typedef struct ceb_buffer_sz {
+	size_t sz;
+	size_t used_sz;
+	size_t *buf;
+	int rsz_ratio;
+} _ceb_buffer_sz_t;
+
+typedef struct ceb_buffer {
+	size_t sz;              // Size of this buffer in multiples of sizeof(char)
+        size_t used_sz;         // Occupied contiguous space inside this buffer.
+	void *buf;              // Beginning of the memory region where this buffer is stored.
+	_ceb_buffer_sz_t types; // Similar buffer structure designed only to contain sequential type sizes as they are contained within this buffer.
+	int rsz_ratio;          // Ratio /100 when the buffer should be resized.
+} ceb_buffer_t;
+
+/* Functions */
+
+char ceb_init_buffer(ceb_buffer_t *buf, size_t init_sz);
+char ceb_append_object(ceb_buffer_t *buf, void *obj_ref, size_t sz);
+void *ceb_get_object(ceb_buffer_t *buf, size_t idx);
+char ceb_remove_object(ceb_buffer_t *buf, size_t idx);
+char ceb_free_buffer(ceb_buffer_t *buf);
+
 #endif /* _H_EXTBUFFER */
