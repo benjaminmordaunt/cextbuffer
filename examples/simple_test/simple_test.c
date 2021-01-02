db96f6af232d7cb1adfa36eb782f88bc114d3c47
Benjamin Crawford <crawford.benjamin15@gmail.com>
2021-01-02 16:45:54 +0000

    Add simple usage example.
    Signed-off-by: Benjamin Crawford <crawford.benjamin15@gmail.com>

---
+++ examples/simple_test/simple_test.c
@@ -0,0 +1,33 @@
+#include "../../include/extbuffer.h"
+#include <assert.h>
+
+int main() {
+	size_t initial_buffer_size = 20; // (20 chars)
+
+	float a = 0.4f;
+	unsigned short b = 2;
+	unsigned char c = 1;
+
+	// Create a new buffer
+	ceb_buffer_t buffer;
+	
+	// Initialise the buffer
+	ceb_init_buffer(&buffer, initial_buffer_size);
+
+	// Add our arbitrarily sized objects using the CEXTARG macro
+	ceb_append_object(&buffer, CEXTARG(a));
+	ceb_append_object(&buffer, CEXTARG(b));
+	ceb_append_object(&buffer, c, sizeof c); // Alternative syntax
+
+	// Remove the 'b' object by index
+	ceb_remove_object(&buffer, 1);
+
+	// Assert that the used buffer size is now sizeof(a) + sizeof(c)
+	assert(buffer.used_sz == (sizeof a) + (sizeof c));
+
+	// Get 'c', which is now located at index 1.
+	unsigned char c_get = (unsigned char)ceb_get_object(&buffer, 1);
+	assert(c_get == c);
+
+	return 0;
+}
