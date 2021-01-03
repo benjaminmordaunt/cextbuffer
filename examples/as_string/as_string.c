abe6cebc95360085c66f5a3b933bbb9311930a00
Benjamin Crawford <crawford.benjamin15@gmail.com>
2021-01-03 21:05:47 +0000

    Add as_string example
    Signed-off-by: Benjamin Crawford <crawford.benjamin15@gmail.com>

---
+++ examples/as_string/as_string.c
@@ -0,0 +1,33 @@
+/* Description:
+ * 	Treat the buffer as a narrow string.
+ * Author:
+ * 	Benjamin Mordaunt
+ * Date:
+ * 	03/01/2020
+ */
+
+#include "../../include/extbuffer.h"
+#include <stdio.h>
+
+int main() {
+	char a = 'H', b = 'i', c = '!';
+
+	// Create a new buffer.
+	ceb_buffer_t buffer = {0};
+
+	// Initialise the buffer with a size of 4.
+	ceb_init_buffer(&buffer, 4);
+
+	// Add the three letters.
+	ceb_append_object(&buffer, CEXTARG(a));
+	ceb_append_object(&buffer, CEXTARG(b));
+	ceb_append_object(&buffer, CEXTARG(c));
+
+	// Interpret the buffer as a string.
+	printf("%s\r\n", buffer.buf);
+
+	// Free the buffer after use.
+	ceb_free_buffer(&buffer);
+
+	return 0;
+}
