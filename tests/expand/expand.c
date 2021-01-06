1e55223c357411154242b29d0dcfd153dc1256d8
Benjamin Crawford <crawford.benjamin15@gmail.com>
2021-01-06 00:52:46 +0000

    Add buffer expansion test
    Signed-off-by: Benjamin Crawford <crawford.benjamin15@gmail.com>

---
+++ tests/expand/expand.c
@@ -0,0 +1,67 @@
+/* Description:
+ * 	This test checks that the size of the buffer doubles once the contents fills more than 50%
+ * Author:
+ * 	Benjamin Mordaunt
+ * Date:
+ * 	06/01/2020
+ */
+
+#include <check.h>
+#include "extbuffer.h"
+
+START_TEST (test_buffer_expansion)
+{
+	char data = 'a';
+
+	// Create a new buffer.
+	ceb_buffer_t buf = {0};
+
+	// Initialise the buffer.
+	ceb_init_buffer(&buf, 10);
+
+	// Specify the resize ratio to be 50%
+	buf.rsz_ratio = 50;
+
+	// Add 4 chars to the buffer and ensure that the size remains the same.
+	for(int i = 0; i < 4; i++) {
+		CEXTPUT(&buf, data); 
+	}
+
+	ck_assert_int_eq(buf.sz, 10);
+
+	// Add 2 more chars and ensure that the size is doubled.
+	for(int i = 0; i < 2; i++) {
+		CEXTPUT(&buf, data);
+	}
+
+	ck_assert_int_eq(buf.sz, 20);
+}
+END_TEST /* test_buffer_expansion */
+
+Suite *suite() {
+	Suite *s;
+	TCase *tc_core;
+
+	s = suite_create("Buffer Behaviour");
+
+	tc_core = tcase_create("Expansion");
+
+	tcase_add_test(tc_core, test_buffer_expansion);
+	suite_add_tcase(s, tc_core);
+
+	return s;
+}
+
+int main() {
+	int number_failed;
+	Suite *s;
+	SRunner *sr;
+
+	s = suite();
+	sr = srunner_create(s);
+
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed != 0);
+}
